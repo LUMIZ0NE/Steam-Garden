3808ecc2f9171b95396fc26bd3537c752f56fcc4
lumis <wrinklyballoon@gmail.com>
2024-06-20 09:21:23 +1200

    Updated attack
    Now has a displacement of 40px from the origin (player) but doesn't maintain when idle

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -7,6 +7,7 @@ const SLASH = preload("res://Scenes/slash.tscn")
 @export var friction = 10
 @onready var player_sprite = $PlayerSprite
 var direction = Vector2.ZERO
+var offset = Vector2.ZERO
 
 func _physics_process(delta):
 	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
@@ -21,10 +22,24 @@ func _physics_process(delta):
 
 	move_and_slide()
 
+func find_direction_x():
+	if direction.x > 0:
+		offset = Vector2(40,0)
+	elif direction.x < 0:
+		offset = Vector2(-40,0)
+
+func find_direction_y():
+	if direction.y > 0:
+		offset = Vector2(0,40)
+	elif direction.y < 0:
+		offset = Vector2(0,-40)
+
 func _input(event):
 	if event.is_action_pressed("attack"):
 		var slash = SLASH.instantiate()
-		slash.global_position = global_position
+		find_direction_x()
+		find_direction_y()
+		slash.global_position = global_position + offset
 		slash.rotate(direction.angle())
 		world.add_child(slash)
 
