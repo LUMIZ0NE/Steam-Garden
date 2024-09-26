5211abab1819c71db573b09663682f48f840d64e
lumis <wrinklyballoon@gmail.com>
2024-09-27 09:48:46 +1200

    added title screen
    project now loads on the title screen
    
    issues present as the file for scores no longer generates causing crashes on score submission

--- Scenes/end_screen.gd
+++ Scenes/end_screen.gd
@@ -1,20 +1,24 @@
 extends Node2D
 var sum_score = 0
-
+var username = "Default Stanley"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if Global.win_check == true:
 		print("WIN!")
 		$ResultBigText.text = "You collected goblin gem"
+		$NameInput.visible = true
+		$EnterScore.visible = true
+		sum_score = Global.player_health / Global.goblins_killed * 5
 	else:
 		print("loss")
 		Global.player_health = 0
 		$Grade.text = "Final grade: F"
+		$NameInput.visible = false
+		$EnterScore.visible = false
 	
 	#display statistics for the run
 	$GoblinKillCount.text = "Goblins killed: " + str(Global.goblins_killed)
 	$HealthLeft.text = "Health remaining: " + str(Global.player_health)
-	sum_score = Global.player_health / Global.goblins_killed * 5 
 	print(str(sum_score))
 	if sum_score == 0:
 		$Grade.text = "Final grade: F"
@@ -31,3 +35,15 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func write_score_to_file():
+	var file = FileAccess.open("user://save_data.dat", FileAccess.READ_WRITE)
+	file.seek_end()
+	file.store_string(username + " " + str(sum_score) + " " + $Grade.text + "\n")
+
+func _on_enter_score_pressed():
+	if $NameInput.text != "":
+		username = $NameInput.text
+	print(username)
+	write_score_to_file()
+	
